91d26d9146470181c512010182cedb4ad6b239a0
Giovanna Carvalho <152983529+giovannacarvalho-tech@users.noreply.github.com>
2026-06-11 20:24:06 -0300

    Add memory leak example in leak.cpp
    Allocate a large block of memory without freeing it.

--- pratica1/leak.cpp
+++ pratica1/leak.cpp
@@ -1,1 +1,16 @@
+#include <iostream>
 
+int main() {
+    size_t tamanho = 1000 * 1024 * 1024;
+    char* memoria = new char[tamanho];
+
+    for (size_t i = 0; i < tamanho; i += 4096) {
+        memoria[i] = 1;
+    }
+
+    std::cout << "Memoria alocada. Pressione Enter...";
+    std::cin.get();
+
+    std::cout << "Memoria NAO liberada. Pressione Enter...";
+    std::cin.get();
+}
