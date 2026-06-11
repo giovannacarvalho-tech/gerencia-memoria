6066bdbefd94b3b91595fe5ac0c9b9eed0a3ec76
Giovanna Carvalho <152983529+giovannacarvalho-tech@users.noreply.github.com>
2026-06-11 20:30:02 -0300

    Allocate and free memory example in memoria.cpp

--- pratica1/memoria.cpp
+++ pratica1/memoria.cpp
@@ -1,1 +1,18 @@
+#include <iostream>
 
+int main() {	
+    size_t tamanho = 1000 * 1024 * 1024;
+    char* memoria = new char[tamanho];
+
+    for (size_t i = 0; i < tamanho; i += 4096) {
+        memoria[i] = 1;
+    }
+
+    std::cout << "Memoria alocada. Pressione Enter...";
+    std::cin.get();
+
+    delete[] memoria;
+
+    std::cout << "Memoria liberada. Pressione Enter...";
+    std::cin.get();
+}
